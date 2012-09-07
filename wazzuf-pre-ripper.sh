4f5feb7b503daaf2008e4831378fb4a033127e83
Nicolas Perrin <booloki@gmail.com>
2012-09-07 12:13:30 +0200

    DVD/ISO pre-rip script improvements + New wazzuf-functions-pre file

--- wazzuf-pre-ripper.sh
+++ wazzuf-pre-ripper.sh
@@ -6,67 +6,24 @@
 CONF_FILE="wazzuf-ripper.conf"
 FUNCTIONS_PATH="wazzuf-ripper-functions"
 FUNCTIONS_CHECK="$FUNCTIONS_PATH/wazzuf-functions-check"
-WAZZUF_FILES=$CONF_FILE
+FUNCTIONS_PRE="$FUNCTIONS_PATH/wazzuf-functions-pre"
+WAZZUF_FILES="$CONF_FILE $FUNCTIONS_PRE"
 
 # check wazzuf files
 source $FUNCTIONS_CHECK
 checkandsource_wazzuf_files
 
 check_mplayer
-check_ffmpeg
 check_avconv
 
 ISO_FILE_PATH=$SOURCE_DIRECTORY/$ISO_FILE
 M2TS_FILE_PATH=$SOURCE_DIRECTORY/$M2TS_FILE
 
-## pre-rip functions
-
-# Run cropdetect to establish the correct crop rectangle
-CROP_FRAMES=100
-cropdetect(){
-	echo -n "Running Crop Detection... "
-	if [[ "$ISO_FILE_PATH" =~ \ |\' ]]
-	then
-		echo -ne "\n *************************************\n"
-		echo " Warning: Path/filename to iso file $ISO_FILE_PATH must not contain spaces for croptetection.."
-		echo -ne " *************************************\n"
-	else
-		mplayer ${1} -vf cropdetect -nosound -vo null -frames $CROP_FRAMES -sstep 1 -nocache &> /tmp/cropdetect.out
-		wait
-		crop=$(cat /tmp/cropdetect.out | tr '\r' '\n' | grep 'vf crop\=' | tail -1 | awk -F\= '{print $2}' | awk -F\) '{print $1}')
-		echo -ne "\nVIDEO_CROP=\"${crop}\"\n"
-		rm /tmp/cropdetect.out
-	fi
-}
-
-	## 'decode' avconv output file
-avconv-decode(){
-	while read line
-	do
-		ID_HEX=`echo ${line} | grep '^Stream' | cut -d '[' -f 2 | cut -d ']' -f 1`
-		if [[ ! $ID_HEX == "" ]]; then
-			echo "ID $(($ID_HEX)) - `echo ${line} | grep ^Stream | cut -d ' ' -f 3-`"
-		fi
-	done  < ${1}
-}
-
-	## 'decode' lsdvd output file
-lsdvd-decode(){
-	while read line
-	do
-		ID_HEX=`echo ${line} | grep -vE '^Title|VTS' | grep -o '.\{5\}$' | sed s/,//`
-		if [[ ! $ID_HEX == "" ]]; then
-			echo "ID $(($ID_HEX)) - `echo ${line} | sed s/', Stream id: ....'//`"
-		fi
-	done  < ${1}
-}
-
-## enter source directory
+# enter source directory
 mkdir -p $SOURCE_DIRECTORY
 cd $SOURCE_DIRECTORY
 
-## pre-rip source choice
-
+# pre-rip source choice
 case $SOURCE in
 BD )
 	if [ ! -f "$M2TS_FILE_PATH" ]
@@ -96,31 +53,52 @@ BD )
 	avconv-decode M2TS-avconv.info
 	;;
 DVD )
+	DVD_PATH="/dev/dvd"
 	check_lsdvd
 	echo -ne "*************************************\n"
-	echo " lsdvd"
- 	lsdvd -acsv /dev/dvd 2>/dev/null > DVD-lsdvd.info
+	echo " lsdvd informations"
+ 	lsdvd -acsv $DVD_PATH 2>/dev/null > DVD-lsdvd.info
 	echo "-> see DVD-lsdvd.info"
 
 	echo -ne "*************************************\n"
-	echo " MPlayer"
-	mplayer -v -vo null -ao null -frames 0 -identify /dev/dvd 2>/dev/null > DVD-mplayer.info
+	echo " MPlayer informations"
+	mplayer -v -vo null -ao null -frames 0 -identify $DVD_PATH 2>/dev/null > DVD-mplayer.info
 	echo "-> see DVD-player.info"
+
+	check_7z
+	echo -ne "*************************************\n"
+	echo " DVD structure informations"
+	7z l $DVD_PATH > DVD-DVDstruct.info
+	echo "-> see ISO-DVDstruct.info"
 
 	echo -ne "*************************************\n"
 	grep "^Disc Title" DVD-lsdvd.info
 	DVD_TITLE_NUMBER=`grep "^Longest track:" DVD-lsdvd.info | sed s/'Longest track: '//`
-	echo "Longest track chosen for crop detection: Title $DVD_TITLE_NUMBER"
-	echo `grep "Title: $DVD_TITLE_NUMBER" DVD-lsdvd.info | cut -d "," -f 2`
-	cropdetect "dvd://$DVD_TITLE_NUMBER"
-	lsdvd -t $DVD_TITLE_NUMBER -acsv /dev/dvd 2>/dev/null | grep -E "Subtitle|Audio|VTS" > DVD-lsdvd-T$DVD_TITLE_NUMBER.info
-	grep VTS DVD-lsdvd-T$DVD_TITLE_NUMBER.info
+	if  [[ $VIDEO_TYPE == "SHOW" && $DVD_EPISODES_ORG == "TITLES" ]]; then
+		for DVD_TITLE_NUMBER in $DVD_TITLE_LIST
+		do
+			echo "######### Title: $DVD_TITLE_NUMBER #########"
+			lsdvd -t $DVD_TITLE_NUMBER -acsv $DVD_PATH 2>/dev/null | grep -E "Subtitle|Audio|VTS" > DVD-lsdvd-T$DVD_TITLE_NUMBER.info
+			echo `grep "Title: " DVD-lsdvd-T$DVD_TITLE_NUMBER.info | cut -d "," -f 2`
+			cropdetect "dvd://$DVD_TITLE_NUMBER"
+			grep VTS DVD-lsdvd-T$DVD_TITLE_NUMBER.info
+			lsdvd-decode DVD-lsdvd-T$DVD_TITLE_NUMBER.info
+		done
+	else
+		echo "Longest track chosen for crop detection: Title $DVD_TITLE_NUMBER"
+		echo `grep "Title: $DVD_TITLE_NUMBER" DVD-lsdvd.info | cut -d "," -f 2`
+		cropdetect "dvd://$DVD_TITLE_NUMBER"
 
-	echo -ne "*************************************\n"
-	echo "Audio/Subtitles tracks (Title $DVD_TITLE_NUMBER):"
-	lsdvd-decode DVD-lsdvd-T$DVD_TITLE_NUMBER.info
+		echo -ne "*************************************\n"
+		echo "Title $DVD_TITLE_NUMBER Tracks:"
+		lsdvd -t $DVD_TITLE_NUMBER -acsv $DVD_PATH 2>/dev/null | grep -E "Subtitle|Audio|VTS" > DVD-lsdvd-T$DVD_TITLE_NUMBER.info
+		grep VTS DVD-lsdvd-T$DVD_TITLE_NUMBER.info
+		lsdvd-decode DVD-lsdvd-T$DVD_TITLE_NUMBER.info
 
-	# copy DVD to iso file ?
+		echo "Note: For multiple Title crop detection, set VIDEO_TYPE to SHOW and DVD_EPISODES_ORG to TITLES"
+	fi
+
+	# choice to copy DVD to iso file
 	DVD_MOUNT_PATH=`mount | grep udf | cut -d ' ' -f 3`
 	DVD_MOUNT_NAME=`echo $DVD_MOUNT_PATH | cut -d '/' -f 3`
 	echo -ne "*************************************\n"
@@ -128,13 +106,11 @@ DVD )
 	read answer
 	case $answer in
 	y* | Y* )
-		check_vobcopy
-		check_mkisofs
-		vobcopy -m -i $DVD_MOUNT_PATH -o $SOURCE_DIRECTORY
-		cd $SOURCE_DIRECTORY
-		mkisofs -allow-limited-size -dvd-video -o $ISO_FILE $DVD_MOUNT_NAME
-		rm -rf $DVD_MOUNT_NAME
+		copy-dvd2iso
 		echo -ne "\n You can now use ISO instead of DVD in configuration file."
+		;;
+	* )
+		echo -ne "Skip DVD copy.\n"
 		;;
 	esac
 	;;
@@ -149,14 +125,14 @@ ISO )
 
 	check_lsdvd
 	echo -ne "*************************************\n"
-	echo " lsdvd"
+	echo " lsdvd informations"
 	lsdvd -acsv "$ISO_FILE_PATH" 2>/dev/null > ISO-lsdvd.info
 	echo "-> see ISO-lsdvd.info"
 	
 	echo -ne "*************************************\n"
 	echo " MPlayer informations"
 	mplayer -v -vo null -ao null -frames 0 -identify "$ISO_FILE_PATH" 2>/dev/null > ISO-mplayer.info
-	echo "-> see ISO-lsdvd.info"
+	echo "-> see ISO-mplayer.info"
 
 	echo -ne "*************************************\n"
 	echo " avconv informations"
@@ -170,19 +146,38 @@ ISO )
 	echo "-> see ISO-DVDstruct.info"
 
 	echo -ne "*************************************\n"
-	echo ISO_FILE_PATH=\"$ISO_FILE_PATH\"
+	echo "Tested iso:\"$ISO_FILE_PATH\""
 	grep "^Disc Title" ISO-lsdvd.info
 	DVD_TITLE_NUMBER=`grep "^Longest track:" ISO-lsdvd.info | sed s/'Longest track: '//`
-	echo "Longest track chosen for crop detection: Title $DVD_TITLE_NUMBER"
-	echo `grep "Title: $DVD_TITLE_NUMBER" ISO-lsdvd.info | cut -d "," -f 2`
-	cropdetect "-dvd-device $ISO_FILE_PATH dvd://$DVD_TITLE_NUMBER"
+	if  [[ $VIDEO_TYPE == "SHOW" && $DVD_EPISODES_ORG == "TITLES" ]]; then
+		for DVD_TITLE_NUMBER in $DVD_TITLE_LIST
+		do
+			echo "######### Title: $DVD_TITLE_NUMBER #########"
+			lsdvd -t $DVD_TITLE_NUMBER -acsv "$ISO_FILE_PATH" 2>/dev/null | grep -E "Subtitle|Audio|VTS" > ISO-lsdvd-T$DVD_TITLE_NUMBER.info
+			echo `grep "Title: " ISO-lsdvd-T$DVD_TITLE_NUMBER.info | cut -d "," -f 2`
+			cropdetect "-dvd-device $ISO_FILE_PATH dvd://$DVD_TITLE_NUMBER"
+			grep VTS ISO-lsdvd-T$DVD_TITLE_NUMBER.info
+			lsdvd-decode ISO-lsdvd-T$DVD_TITLE_NUMBER.info
+		done
+	else
+		echo "Longest track chosen for crop detection: Title $DVD_TITLE_NUMBER"
+		echo `grep "Title: $DVD_TITLE_NUMBER" ISO-lsdvd.info | cut -d "," -f 2`
+		cropdetect "-dvd-device $ISO_FILE_PATH dvd://$DVD_TITLE_NUMBER"
 
-	echo -ne "*************************************\n"
-	echo "Audio/Subtitles tracks:"
-	avconv-decode ISO-avconv.info
+		echo -ne "*************************************\n"
+		echo "Title $DVD_TITLE_NUMBER Tracks:"
+		lsdvd -t $DVD_TITLE_NUMBER -acsv "$ISO_FILE_PATH" 2>/dev/null | grep -E "Subtitle|Audio|VTS" > ISO-lsdvd-T$DVD_TITLE_NUMBER.info
+		grep VTS ISO-lsdvd-T$DVD_TITLE_NUMBER.info
+		lsdvd-decode ISO-lsdvd-T$DVD_TITLE_NUMBER.info
+
+		# not so interesting
+		#avconv-decode ISO-avconv.info
+
+		echo "Note: For multiple Title crop detection, set VIDEO_TYPE to SHOW and DVD_EPISODES_ORG to TITLES"
+	fi
 	;;
 * )
-        echo -ne "\n Media source problem: make sure you filled DVD ISO BD in configuration file\n"
+        echo -ne "\n Media source problem: make sure you filled DVD, ISO or BD in configuration file\n"
         exit 1
         ;;
 esac
